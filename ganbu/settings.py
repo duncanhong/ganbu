184e23f963641770f4a2b04535272e225d4b1ce4
Chisheng Hong <chisheng@Chishengs-MacBook-Pro-2.local>
2017-03-24 01:26:15 -0700

    Add static files and fix bugs for poll example

--- ganbu/settings.py
+++ ganbu/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'ganbu',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': '3306'
     }
--- polls/views.py
+++ polls/views.py
@@ -14,8 +14,8 @@ def index(request):
 
 
 def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question.filter(pub_date__lte=timezone.now())})
+    question = get_object_or_404(Question, pk=question_id, pub_date__lte=timezone.now())
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
